2ab8cf9108394dbe4933cdc04caf6dd470c9d073
KenKindom <ken830820@gmail.com>
2019-03-15 11:52:41 +0800

    Create 122. Best Time to Buy and Sell Stock II.cpp

---
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,17 @@
+//C++ Code
+//Title      Best Time to Buy and Sell Stock II
+//Difficulty Easy
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int res = 0, n = prices.size();
+        for (int i = 0; i < n - 1; ++i) 
+        {
+            if (prices[i] < prices[i + 1])
+                res += prices[i + 1] - prices[i];
+        }
+        return res;
+    }
+};
