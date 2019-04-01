de4fa32e9eeab499efde09f0cd51206c2f4134b5
KenKindom <ken830820@gmail.com>
2019-04-01 09:38:45 +0800

    Create 198. House Robber.cpp

---
+++ 198. House Robber.cpp
@@ -0,0 +1,17 @@
+//C++ Code
+//Title      House Robber
+//Difficulty Easy
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int rob = 0, notRob = 0, n = nums.size();
+        for (int i = 0; i < n; ++i) 
+        {
+            int preRob = rob, preNotRob = notRob;
+            rob = preNotRob + nums[i];
+            notRob = max(preRob, preNotRob);
+        }
+        return max(rob, notRob);
+    }
+};
