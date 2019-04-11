492711996f30de749289dcd17e878a770e5dfae2
KenKindom <ken830820@gmail.com>
2019-04-11 10:05:10 +0800

    Create 268. Missing Number.cpp

---
+++ 268. Missing Number.cpp
@@ -0,0 +1,17 @@
+//C++ Code
+//Title      Missing Number
+//Difficulty Easy
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size()-1; i++)
+        {
+            if(nums[i] != nums[i+1]-1)
+                return nums[i]+1;
+        }
+        
+        return nums[0] == 0 ? nums[nums.size()-1]+1 : nums[0]-1;
+    }
+};
