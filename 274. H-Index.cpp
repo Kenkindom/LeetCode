ecf703fb1effd5441243c58a5c26489eea8d1e53
KenKindom <ken830820@gmail.com>
2019-04-12 09:39:56 +0800

    Create 274. H-Index.cpp

---
+++ 274. H-Index.cpp
@@ -0,0 +1,16 @@
+//C++ Code
+//Title      H-Index
+//Difficulty Medium
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+        for (int i = 0; i < citations.size(); ++i) 
+        {
+            if (i >= citations[i]) 
+                return i;
+        }
+        return citations.size();
+    }
+};
