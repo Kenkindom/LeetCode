8fc1749b79d6d8091996e5c8814e787edb4f588b
KenKindom <ken830820@gmail.com>
2019-04-18 10:54:56 +0800

    Create 389. Find the Difference.cpp

---
+++ 389. Find the Difference.cpp
@@ -0,0 +1,17 @@
+//C++ Code
+//Title      Find the Difference
+//Difficulty Easy
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] != t[i])
+                return t[i];
+        }
+        return t[t.size()-1];
+    }
+};
