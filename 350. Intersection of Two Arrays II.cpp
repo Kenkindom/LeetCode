0eb6a95e14268510eb9dfd319fbccfd0a1f106b1
KenKindom <ken830820@gmail.com>
2019-04-21 10:12:39 +0800

    Create 350. Intersection of Two Arrays II.cpp

---
+++ 350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,18 @@
+//C++ Code
+//Title      Intersection of Two Arrays II
+//Difficulty Easy
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> m;
+        vector<int> res;
+        for (auto a : nums1) 
+            ++m[a];
+        for (auto a : nums2) {
+            if (m[a]-- > 0) 
+                res.push_back(a);
+        }
+        return res;
+    }
+};
