f7b1453eaad214b901f5c34c7a45cf974a29ce80
KenKindom <ken830820@gmail.com>
2019-04-25 10:30:19 +0800

    Create 395. Longest Substring with At Least K Repeating Characters.cpp

---
+++ 395. Longest Substring with At Least K Repeating Characters.cpp
@@ -0,0 +1,32 @@
+//C++ Code
+//Title      Longest Substring with At Least K Repeating Characters
+//Difficulty Medium
+
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        // Reference From https://reurl.cc/Wbbne
+        int res = 0, i = 0, n = s.size();
+        while (i + k <= n) 
+        {
+            int m[26] = {0}, mask = 0, max_idx = i;
+            for (int j = i; j < n; ++j) 
+            {
+                int t = s[j] - 'a';
+                ++m[t];
+                if (m[t] < k) 
+                    mask |= (1 << t);
+                else 
+                    mask &= (~(1 << t));
+                
+                if (mask == 0) 
+                {
+                    res = max(res, j - i + 1);
+                    max_idx = j;
+                }
+            }
+            i = max_idx + 1;
+        }
+        return res;
+    }
+};
