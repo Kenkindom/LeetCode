037656dd50891e5d7987d3ad9260f4dc09a0e6f3
KenKindom <ken830820@gmail.com>
2019-04-26 08:02:47 +0800

    Create 412. Fizz Buzz.cpp

---
+++ 412. Fizz Buzz.cpp
@@ -0,0 +1,22 @@
+//C++ Code
+//Title      Fizz Buzz
+//Difficulty Easy
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> str;
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % 15 == 0)
+                str.push_back("FizzBuzz");
+            else if(i % 5 == 0)
+                str.push_back("Buzz");
+            else if(i % 3 == 0)
+                str.push_back("Fizz");
+            else
+                str.push_back(to_string(i));
+        }
+        return str;
+    }
+};
