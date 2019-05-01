990dd8ad2d99c0e7c6d764fd30c07a639e5faa49
KenKindom <ken830820@gmail.com>
2019-05-01 08:40:28 +0800

    Create 342. Power of Four.cpp

---
+++ 342. Power of Four.cpp
@@ -0,0 +1,10 @@
+//C++ Code
+//Title      Power of Four
+//Difficulty Easy
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        return num > 0 && !(num & (num - 1)) && (num & 0x55555555) == num;
+    }
+};
