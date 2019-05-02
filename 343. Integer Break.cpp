34a25d879b894c091d12bc16a8926d11723fcf64
KenKindom <ken830820@gmail.com>
2019-05-02 10:28:38 +0800

    Create 343. Integer Break.cpp

---
+++ 343. Integer Break.cpp
@@ -0,0 +1,18 @@
+//C++ Code
+//Title      Integer Break
+//Difficulty Medium
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2 || n == 3) 
+            return n - 1;
+        int res = 1;
+        while (n > 4) 
+        {
+            res *= 3;
+            n -= 3;
+        }
+        return res * n;
+    }
+};
